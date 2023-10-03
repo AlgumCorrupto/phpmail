1d2b23f177f17a206c2f65be45edff193bc88b02
Paulo <pauloarturvillaca@gmail.com>
2023-10-03 18:18:38 -0300

    ferramenta de envio feita

--- sendmail.php
+++ sendmail.php
@@ -1,16 +1,46 @@
 <?php 
+# FEITO POR: Paulo Artur 
+# 03/10/2023
+
+# OBS: No gmail ativar 'Acesso a app menos seguro'
+
+//include classes
 use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-require 'path/to/PHPMailer/src/Exception.php';
-require 'path/to/PHPMailer/src/PHPMailer.php';
-require 'path/to/PHPMailer/src/SMTP.php';
-# ini_set("SMTP", "smtp.gmail.com");
-# $from = $_POST["from"];
-$to = $_POST["to"];
-$head = $_POST["head"];
-$body = $_POST["body"];
+require './vendor/autoload.php';
 
-mail($to, $head, $body);
+//criar objeto do handler do email e habilitar exceções (true)
+$mail = new PHPMailer(true);
+$to   = $_POST['to'];
+$head = $_POST['head'];
+$body = $_POST['body'];
 
+try {
+    // configs do server
+    $mail->isSMTP();
+    $mail->Host       = 'smtp.gmail.com';                    # server host
+    $mail->SMTPAuth   = true;                                # auth
+    $mail->Username   = 'seu@email.com';                     # email
+    $mail->Password   = 'secreto';                           # senha
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;         # habilitar encripção
+    $mail->Port       = 465;                                 # porta usada
+
+    // Recipientes
+    $mail->setFrom('seu@email.com');
+    $mail->addAddress($to);
+
+    // Conteúdo
+    $mail->isHTML(false);
+    $mail->Subject = $head;
+    $mail->Body    = $body;
+
+    $mail->send();
+    echo "<h1>Mensagem mandada!</h1>";
+}
+catch (Exception $e) {
+    echo "<h1>Mensagem <b>NÃO</b> mandada!</h1>".
+    "<h2>Erro: $mail->ErrorInfo</h2>";
+}
 ?>
